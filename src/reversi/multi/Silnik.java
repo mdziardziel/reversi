fe1c403da2e5d1a27499cc74d3bbf0ff78d53b4b
mdziardziel <mdziardziel@gmail.com>
2018-01-07 14:38:16 +0100

    cos tam dziala, ale bledy jeszcze sa

--- src/reversi/multi/Silnik.java
+++ src/reversi/multi/Silnik.java
@@ -26,23 +26,41 @@ public class Silnik {
         tabela[4][3] = 1;
     }
     
+    
+    
     private static boolean zawiera(int p1, int p2, int pp){
         if(p1<=pp && pp<=p2)return true;
         if(p1>=pp && pp>=p2)return true;
         return false;
     }
     
+    private static boolean zawieraSkos(int x1, int x2, int x3, int y1, int y2, int y3){
+        while(x1!=x2 && y1!=y2){
+            if(x1 == x3 && y1 == y3)return true;
+            if(x1==x2 && y1==y2)return false;
+            if(x1<x2)x1++;else x1--;
+            if(y1<y2)y1++;else y1--;
+        }
+        return false;
+    }
+    
+    
+    
     public static void koloruj(int x, int y, int nx, int ny){
-        //System.out.println(x + " "+ y + " "+ nx + " "+ ny);
+        System.out.println(x + " "+ y + " "+ nx + " "+ ny);
         for(int i = 0; i < 8; i++){
             for(int j = 0; j < 8; j++){
-                if(zawiera(x,nx,j) && zawiera(y,ny,i)) {
+                if((x==nx || y==ny) && zawiera(x,nx,j) && zawiera(y,ny,i)) {
                     tabela[i][j] = ruch;
                     //System.out.println(j+ " " + i);
+                }else if((x!=nx && y!=ny) && zawieraSkos(x,nx,j,y,ny,i)){
+                    tabela[i][j] = ruch;
                 }
             }
         }
     }
+    
+    
     
     public static void zmiana(){
         int pom = ruch;
@@ -52,15 +70,20 @@ public class Silnik {
         reversi.multi.Multi.okno.resetPionkow();
     }
     
+    
+    
+    
+    
     public static boolean sprawdzRuch(int x, int y){
         boolean ret = false;
+        if(tabela[y][x] > 0 ) return false;
         
         //gora
         for(int i = y - 2; i >=0; i-- ){
             if(tabela[i+1][x]==0 || tabela[i+1][x]==ruch) break;
             if(tabela[i][x]== 0) break;
             if(tabela[i][x]== ruch){
-                koloruj(x,y,x,i+1);
+                koloruj(x,y,x,i);
                 ret = true;
                 break;
             }
@@ -70,7 +93,7 @@ public class Silnik {
             if(tabela[i-1][x]==0 || tabela[i-1][x]==ruch) break;
             if(tabela[i][x]== 0) break;
             if(tabela[i][x]== ruch){
-                koloruj(x,y,x,i-1);
+                koloruj(x,y,x,i);
                 ret = true;
                 break;
             }
@@ -80,7 +103,7 @@ public class Silnik {
             if(tabela[y][i-1]==0 || tabela[y][i-1]==ruch) break;
             if(tabela[y][i]== 0) break;
             if(tabela[y][i]== ruch){
-                koloruj(x,y,i-1,y);
+                koloruj(x,y,i,y);
                 ret = true;
                 break;
             }
@@ -90,13 +113,73 @@ public class Silnik {
             if(tabela[y][i+1]==0 || tabela[y][i+1]==ruch) break;
             if(tabela[y][i]== 0) break;
             if(tabela[y][i]== ruch){
-                koloruj(x,y,i-1,y);
+                koloruj(x,y,i,y);
                 ret = true;
                 break;
             }
         }
+        
+        //SKOSY
+        int i = x, j = y;
+        
+        //lewo gora
+        i= x -2; j= y - 2;
+        while(i>=0 && j >=0){
+            if(tabela[j+1][i+1]==0 || tabela[j+1][i+1]==ruch) break;
+            if(tabela[j][i]== 0) break;
+            if(tabela[j][i]== ruch){
+                koloruj(x,y,i,j);
+                ret = true;
+                break;
+            }
+            i--;j--;
+        }
+        
+        //lewo dol
+        i = x -2; j = y + 2;
+        while(i>=0 && j<8){
+            if(tabela[j-1][i+1]==0 || tabela[j-1][i+1]==ruch) break;
+            if(tabela[j][i]== 0) break;
+            if(tabela[j][i]== ruch){
+                koloruj(x,y,i,j);
+                ret = true;
+                break;
+            }
+            i--;j++;
+        }
+        
+        //prawo gora
+        i = x + 2; j = j - 2;
+        while(i<8 && j >=0){
+            if(tabela[j+1][i-1]==0 || tabela[j+1][i-1]==ruch) break;
+            if(tabela[j][i]== 0) break;
+            if(tabela[j][i]== ruch){
+                koloruj(x,y,i,j);
+                ret = true;
+                break;
+            }
+            i++;j--;
+        }
+        
+        //prawo dol
+        i = x + 2; j = y - 2;
+        while(i < 8 && j < 8){
+            if(tabela[j-1][i-1]==0 || tabela[j-1][i-1]==ruch) break;
+            if(tabela[j][i]== 0) break;
+            if(tabela[j][i]== ruch){
+                koloruj(x,y,i,j);
+                ret = true;
+                break;
+            }
+            i++;j++;
+        }
+        
         return ret;
     }
+    
+    
+    
+    
     
     public static int getRuch(){
         return ruch;
